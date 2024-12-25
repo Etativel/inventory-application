972a5c11ada0b682b76c57b6ff9d1e9d4e1f9320
Etativel <farhanmaulana4611@gmail.com>
2024-12-25 13:55:35 +0800

    feat: add controllers and routes file

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,11 @@
 const pool = require("./pool");
+
+async function getAllCategory() {
+  const { rows } = await pool.query("SELECT * FROM category_table");
+  return rows;
+}
+
+async function getAllProducts() {
+  const { rows } = await pool.query("SELECT * FROM inventory_table");
+  return rows;
+}
